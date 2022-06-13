bbec82a065313c58b62e937c3ea8f844d3c9d9a5
CristiGitHub <lefterioancristian@gmail.com>
2022-06-13 20:16:45 +0300

    Unit Test Example implemented

---
+++ src/test/resources/sql/data.sql
@@ -0,0 +1,6 @@
+Delete from requester;
+Delete from campaign;
+Delete from donation;
+
+INSERT INTO public.requester (id, birthday, email, name) VALUES ('1', '2022-06-02', 'test@test.com', 'test');
+
